9f1e48a90b83a8bcc9e6b14bb57e14f842f89bdb
pavluk <pavlukan1982@gmail.com>
2016-02-17 16:38:18 +0300

    Added examples

--- jira-app/src/test/java/by.pavlyukevich/jira/SimpleTest.java
+++ jira-app/src/test/java/by.pavlyukevich/jira/SimpleTest.java
@@ -2,8 +2,10 @@ package by.pavlyukevich.jira;
 
 import com.atlassian.jira.rest.client.api.IssueRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.MetadataRestClient;
 import com.atlassian.jira.rest.client.api.domain.BasicIssue;
 import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.IssueType;
 import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
 import com.atlassian.jira.rest.client.api.domain.input.IssueInputBuilder;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
@@ -12,10 +14,12 @@ import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -23,60 +27,44 @@ import static com.google.common.collect.Iterables.transform;
 
 public class SimpleTest {
 
-    final static Logger logger = Logger.getLogger(SimpleTest.class);
+    final private static Logger logger = Logger.getLogger(SimpleTest.class);
+    final public static JiraRestClient restClient;
+    static {
+        URI jiraServerUri = URI.create("https://testjirainstance1.atlassian.net");
+        AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+        restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "admin", "testjira");
+    }
 
     @Test
-    public void test() throws IOException{
-        logger.error("OK");
+    public void testAddIssue() throws IOException{
+        logger.debug("Start creating issue");
 
-        URI jiraServerUri = URI.create("https://testjirainstance1.atlassian.net");
-
-        final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
-        final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "admin", "testjira");
-
-        try {
-            final List<Promise<BasicIssue>> promises = Lists.newArrayList();
-            final IssueRestClient issueClient = restClient.getIssueClient();
-
-            System.out.println("Sending issue creation requests...");
-            for (int i = 0; i < 1; i++) {
-                final String summary = "NewIssue#" + i;
-                final IssueInput newIssue = new IssueInputBuilder("TJP", 10002L, summary).build();
-                System.out.println("\tCreating: " + summary);
-                promises.add(issueClient.createIssue(newIssue));
-            }
-
-            System.out.println("Collecting responses...");
-            final Iterable<BasicIssue> createdIssues = transform(promises, new Function<Promise<BasicIssue>, BasicIssue>() {
-                @Override
-                public BasicIssue apply(Promise<BasicIssue> promise) {
-                    return promise.claim();
-                }
-            });
-
-            System.out.println("Created issues:\n" + Joiner.on("\n").join(createdIssues));
-        } finally {
-            restClient.close();
-        }
+        IssueRestClient issueClient = restClient.getIssueClient();
+        IssueInput issueInput = new IssueInputBuilder("TJP", 10002L, "New issue 1").build();
+        Promise<BasicIssue> issue = issueClient.createIssue(issueInput);
+        issue.claim();
     }
 
     @Test
     public void testGetIssue() {
-        URI jiraServerUri = URI.create("https://testjirainstance1.atlassian.net");
-
-        final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
-        final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "admin", "testjira");
-
         final IssueRestClient issueClient = restClient.getIssueClient();
         Promise<Issue> issue = issueClient.getIssue("TJP-1");
-        try {
-            issue.get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
-
+        System.out.println(issue.claim());
     }
+
+    @Test
+    public void testGetAllIssueTypes() {
+        MetadataRestClient metadataClient = restClient.getMetadataClient();
+        Promise<Iterable<IssueType>> issueTypes = metadataClient.getIssueTypes();
+        Iterator<IssueType> iterator = issueTypes.claim().iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    @After
+    public void closeRestClient() throws IOException{
+        restClient.close();
+    }
+
 }
